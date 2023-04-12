7aea5d67008f3f84ceca333549a9e8800dc680ba
Jonathan C MacMillan <jmacmill@gmail.com>
2023-04-12 15:30:22 +1000

    Add skeleton component structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+
 function App() {
     return <div>
-        App
+        <CarForm />
+        <CarList />
+        <CarSearch />
+        <CarValue />
     </div>
 }
 
