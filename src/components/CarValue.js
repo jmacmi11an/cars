e6a42def6a9717924520660e72b7ce044a281129
Jonathan C MacMillan <jmacmill@gmail.com>
2023-04-17 15:42:59 +1000

    Refactor totalCost variable

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux';
 
 function CarValue() {
-    const totalCost = useSelector(({cars: {data, searchTerm}}) => {
-        return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-        .reduce((acc, car) => acc + car.cost, 0)
-    });
+    const totalCost = useSelector(({cars: {data, searchTerm}}) => 
+        data
+            .filter((car) => 
+                car.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .reduce((acc, car) => acc + car.cost, 0)
+    );
 
     return (
         <div className="car-value">
